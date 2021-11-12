1455c0e4376ee4f1e2a35f7cef0c7538d0a0a06c
jdvv2005 <93164049+jdvv2005@users.noreply.github.com>
2021-11-11 18:35:06 -0600

    Add files via upload

---
+++ Plantilla-del-Proyecto-C13-main/JUEGO DEL CONEJO.js
@@ -0,0 +1,56 @@
+var garden,rabbit;
+var gardenImg,rabbitImg;
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("apple.png");
+ orangeImg = loadImage("orangeLeaf.png");
+  redImg = loadImage("redImage.png"); 
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// Mover el fondo
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+//crear sprite de rabbit (conejo)
+rabbit = createSprite(180,340,30,30);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+}
+function creatApples(){
+  apple = createSprite(random(50,350),40,10,10);
+  apple.addImage(appleImg);
+  apple.scale=0.07;
+  apple.velocity = 3;
+  apple.lifetime = 150;
+
+}
+function createOrange(){
+orangeL = createSprite(random(50,350),40,10,10);
+orangeL.addImage(orangeImg);
+orangeL.scale=0.08;
+orangeL.velocityY= 3;
+orangeL.lifetime = 150;
+}
+function createRed(){
+redL = createSprite(random(50,350),40,10,10);
+redL.addImage(redImg);
+redL.scale=0.06;
+ red.velocityY = 3;
+ redL.lifetime = 150;
+}
+function draw() {
+  background(0);
+
+
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+
+  drawSprites();
+  
+}
